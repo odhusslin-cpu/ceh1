ae5abdac6ec328c903f9d7326d6cab68695c9d6c
odhusslin-cpu <odhusslin@gmail.com>
2026-08-24 23:46:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const C = "ceh-v3";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(n => n !== C).map(n => caches.delete(n)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(C).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
