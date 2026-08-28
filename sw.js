cf748e1cbd7f31297301e1e75f459997a7886431
odhusslin-cpu <odhusslin@gmail.com>
2026-08-28 15:30:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "ceh-v6";
+const C = "ceh-v8";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
